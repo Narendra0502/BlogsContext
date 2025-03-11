0f2b289cac41c835af6e48f52d91cb9c5d07f6e8
Narendra Kaushik <156305888+Narendra0502@users.noreply.github.com>
2025-03-12 02:36:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-export default function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Blog/>
-      <Pageination/>
+import React from 'react';
+import Header from './components/Header'; // Adjust the paths as necessary
+import Blog from './components/Blog';
+import Pageination from './components/Pageination';
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <Blog />
+      <Pageination />
     </div>
-  )
+  );
 }
+
+export default App;
